844fdb14076b1399bb1e50087838cd662d3ab087
guangyao-d <guangyao@gmx.com>
2022-10-24 22:07:49 +0800

    小改

--- app/app.py
+++ app/app.py
@@ -1,19 +1,29 @@
 import logging
+import os
 import sys
+from mgr.pluginmgr import PluginMGR
 def InitLog(is_Debug=False):
     global logger
     logger = logging.getLogger("main")
-    logger.setLevel(level = logging.INFO)
-    handler = logging.FileHandler("app.log",maxBytes = 1*1024,backupCount = 3)
-    handler.setLevel(logging.INFO)
+    lvl_log = logging.INFO
+    lvl_cons = logging.WARN
+    if is_Debug:
+        lvl_log = logging.DEBUG
+        lvl_cons = logging.DEBUG
+    logger.setLevel(level = lvl_log)
+    if not 'logs' in os.listdir("."):
+            os.mkdir("logs")
+    handler = logging.FileHandler("./logs/app.log")
+    handler.setLevel(lvl_log)
     formatter = logging.Formatter('(%(levelname)s)[%(asctime)s][%(name)s] %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     console = logging.StreamHandler()
-    console.setLevel(logging.WARN)
+    console.setLevel(lvl_cons)
     console.setFormatter(formatter)
     logger.addHandler(console)
     logger.debug("Successfully Initialed Logger.")
 
 if __name__ == "__main__":
-    InitLog
+    InitLog(is_Debug=True)
+    Plugins = PluginMGR()
--- app/mgr/pluginmgr.py
+++ app/mgr/pluginmgr.py
@@ -9,12 +9,21 @@ class Plugin(object):
         self.logger = logging.getLogger(self.namespace)
         self.logger.debug("Loading Plugin %s.".format(self.namespace))
         return
-    def registerHandler(self,)
+    def registerHandler(self):
+        pass
 
 class PluginMGR:
     def __init__(self):
         self.logger = logging.getLogger("PluginMGR")
         self.logger.info("Loading...")
+        self.loaded_plugins = []
+        if not 'plugins' in os.listdir("."):
+            os.mkdir("plugins")
+        plugins = os.listdir("./plugins")
+        for item in plugins:
+            if os.path.isfile(os.path.abspath("./plugins/"+item)):
+                self.loaded_plugins.append(__import__(os.path.abspath("./plugins/"+item)))
+
 
     def register(self):
         os.path.get()
